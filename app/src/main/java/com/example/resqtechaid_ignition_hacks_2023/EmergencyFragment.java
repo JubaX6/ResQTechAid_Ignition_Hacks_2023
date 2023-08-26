f5fbf12458aa9fd1c927d978a3fc1ef163e01562
JubaX6 <jubaales@gmail.com>
2023-08-26 18:24:14 -0400

    LearnMore UI fixed

--- app/src/main/java/com/example/resqtechaid_ignition_hacks_2023/EmergencyFragment.java
+++ app/src/main/java/com/example/resqtechaid_ignition_hacks_2023/EmergencyFragment.java
@@ -8,33 +8,27 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.VideoView;
+import android.widget.MediaController;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link EmergencyFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
 public class EmergencyFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
     Button buttonLocation;
+    Button btnCprProcedure;
+    VideoView videoView;
 
     private static final int LOCATION_PERMISSION_CODE = 1;
 
@@ -42,15 +36,6 @@ public class EmergencyFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment EmergencyFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static EmergencyFragment newInstance(String param1, String param2) {
         EmergencyFragment fragment = new EmergencyFragment();
         Bundle args = new Bundle();
@@ -72,25 +57,26 @@ public class EmergencyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_emergency, container, false);
 
         buttonLocation = rootView.findViewById(R.id.buttonLocation);
+        btnCprProcedure = rootView.findViewById(R.id.btnCprProcedure);
+        videoView = rootView.findViewById(R.id.videoView);
+
+        final MediaController mediaController = new MediaController(requireContext());
+        mediaController.setAnchorView(videoView);
+
         buttonLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Check for location permission
                 if (ContextCompat.checkSelfPermission(requireContext(),
                         Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    // Get user's current location
                     LocationManager locationManager = (LocationManager) requireActivity().getSystemService(Context.LOCATION_SERVICE);
                     Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
                     if (location != null) {
                         double latitude = location.getLatitude();
                         double longitude = location.getLongitude();
-
-                        // Open Google Maps with nearest hospital location
                         String uri = "http://maps.google.com/maps?q=nearest+hospital&loc:" + latitude + "," + longitude;
                         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
                         startActivity(intent);
@@ -98,11 +84,21 @@ public class EmergencyFragment extends Fragment {
                         Toast.makeText(requireContext(), "Unable to get current location.", Toast.LENGTH_SHORT).show();
                     }
                 } else {
-                    // Request location permission
                     ActivityCompat.requestPermissions(requireActivity(),
                             new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                             LOCATION_PERMISSION_CODE);
                 }
+            }
+        });
+
+        btnCprProcedure.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.youtube.com/watch?v=pxDWQlRFBSE&ab_channel=RushUniversitySystemforHealth");
+                videoView.setMediaController(mediaController);
+                videoView.setVideoURI(uri);
+                videoView.requestFocus();
+                videoView.start();
             }
         });
 
